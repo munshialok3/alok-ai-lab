dc5af211e2077284377db2dc4e3801ce110cd3f2
Alok Munshi <munshialok3@gmail.com>
2026-04-28 11:56:10 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import ScrollToTop from './ScrollToTop'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const BASE_URL = 'https://alok-munshi-portfolio.vercel.app'
